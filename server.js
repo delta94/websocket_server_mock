8860a51fe951639b672a19be4e357cc44bc0f01f
abteilung6 <moeller.tilman@gmail.com>
2020-11-28 12:46:03 +0100

    log connection details

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const WebSocket = require("ws");
 
 const wss = new WebSocket.Server({ port: 3030 });
 
-wss.on("connection", function connection(ws) {
+wss.on("connection", function connection(ws, req) {
+  console.log("new connection: " + req.connection.remoteAddress);
   ws.on("message", function incoming(data) {
+    console.log(req.connection.remoteAddress + " send: " + data);
     wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(data);
